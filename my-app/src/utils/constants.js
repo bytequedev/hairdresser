70a7a169c738aa5c79062145c1f32b0adc8ef626
Burçin Güngör <burcin.gngr.gs@gmail.com>
2025-11-03 00:49:15 +0300

    new

---
+++ my-app/src/utils/constants.js
@@ -0,0 +1,16 @@
+export const COLORS = {
+  primary: "#DF2F80",       
+  background: "#FFECF6",    //background 
+  second: "#FFF8FC",        
+  white: "#ffffffff",   
+  black: "#000000ff",  
+  wp:"#45DD4D"
+};
+
+
+export const SIZES = {
+  small: 12,
+  medium: 16,
+  large: 20,
+  xLarge: 24,
+};
