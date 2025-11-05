d1908e9095b6ec111819a04ca7e02f93f2b11f1f
Başak Köseoğlu <basakoseoglu@gmail.com>
2025-11-05 19:59:45 +0300

    logo updated

--- my-app/src/components/FooterPage/FooterLogo.jsx
+++ my-app/src/components/FooterPage/FooterLogo.jsx
@@ -7,10 +7,10 @@ const FooterLogo = () => {
     <div className="col-12 col-sm-6 col-md-3 mb-4">
       <div className="d-flex align-items-center mb-3">
         <img
-          src="/assets/eliflogo.png"
+          src="/assets/seffaflogo.png"
           alt="logo"
           className="img-fluid"
-          style={{ width: "90px", height: "50px", borderRadius: "50%" }}
+          style={{ width: "200px", height: "auto", borderRadius: "50%" }}
         />
         <div className="ms-2">
           <h6 className="text-color fw-bold mb-0">ELİF İMZAOĞLU</h6>
--- my-app/src/components/HomePage/LogoSection.jsx
+++ my-app/src/components/HomePage/LogoSection.jsx
@@ -4,11 +4,12 @@ const LogoSection = () => {
   return (
     <div className="col-12 col-lg-6 text-center order-0 order-lg-1">
       <div className="d-flex justify-content-center justify-content-lg-center">
-        <div className="position-relative">
+        <div className="position-relative logo-style">
           <img
-            src="/assets/eliflogo.png"
+            src="/assets/seffaflogo.png"
             title="Elif Kuaför - Randevu Al!"
             className="img-fluid logo"
+           
           />
         </div>
       </div>
